e7de13ab829a70dc91607a86077bd747430c7729
Jayson Nutt <jayson.nutt@gmail.com>
2025-12-16 21:28:51 -1000

    update

--- init.lua
+++ init.lua
@@ -1,16 +1,52 @@
+vim.g.mapleader=" "
+
+-- LazyVIM Stuff
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {}
+local plugins = {
+  { "catppuccin/nvim", name="catppuccin", pirority=1000 },
+  {
+    'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
+     dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+}
+
+require("lazy").setup(plugins, opts)
+require("catppuccin").setup()
+
+--- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
 vim.cmd([[
 
-  " Vim Plugs
-  call plug#begin(stdpath('data') . '/plugged')
-  
-    Plug 'preservim/nerdtree'
-    Plug 'iamcco/markdown-preview.nvim', { 'do': { -> mkdp#util#install() }, 'for': ['markdown', 'vim-plug']}
-  
-  call plug#end()
-
-  " Vim Plug Mappings
-  nnoremap <leader>t :NERDTreeToggle<CR>
-  nnoremap <leader>p :MarkdownPreviewToggle<CR>
+  " Markdown Preview Bindings
+  let g:mkdp_auto_start=1
+  let g:mkdp_auto_close=1
+  nmap <C-s> <Plug>MarkdownPreview
+  nmap <M-s> <Plug>MarkdownPreviewStop
+  nmap <C-p> <Plug>MarkdownPreviewToggle
 
   " Standard things
   set number
@@ -48,5 +84,5 @@ vim.cmd([[
   nnoremap <c-j> <c-w>j
   nnoremap <c-k> <c-w>k
   
-  colorscheme habamax
+  colorscheme catppuccin
 ]])
