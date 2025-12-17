f8f931dd0117d4c4098707dce953ed4476b3bf9a
Jayson Nutt <jayson.nutt@gmail.com>
2025-12-16 21:44:47 -1000

    update

--- init.lua
+++ init.lua
@@ -44,9 +44,6 @@ vim.cmd([[
   " Markdown Preview Bindings
   let g:mkdp_auto_start=1
   let g:mkdp_auto_close=1
-  nmap <C-s> <Plug>MarkdownPreview
-  nmap <M-s> <Plug>MarkdownPreviewStop
-  nmap <C-p> <Plug>MarkdownPreviewToggle
 
   " Standard things
   set number
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install packages
-dnf install -y fastfetch rofi kitty xrdp neovim flameshot distrobox podman gimp feh
+dnf install -y fastfetch rofi kitty xrdp neovim flameshot distrobox podman gimp feh konsole
 
 # i3 Configuration
 mkdir -p ~/.config/i3/
